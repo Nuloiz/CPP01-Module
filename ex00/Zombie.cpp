723f792a22e9da70a3651eeeae7150e816132c99
nschutz <nschutz@student.42wolfsburg.de>
2024-01-26 14:44:08 +0100

    feat: CPP01 ex00 compiles but segfaults but now fancier

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -4,6 +4,6 @@ void Zombie::announce(void){
 	std::cout << name << ": BraiiiiiiinnnzzzZ...";
 }
 
-void Zombie::set_name(std::string name){
-	Zombie::name = name;
+void Zombie::set_name(std::string string){
+	name = string;
 }
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -7,7 +7,7 @@
 class Zombie{
 	public:
 		void	announce(void);
-		void 	set_name(std::string name);
+		void 	set_name(std::string string);
 	private:
 		std::string name;
 
