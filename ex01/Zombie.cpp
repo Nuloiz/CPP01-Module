f363c6ec4b34046d57f3ec6feab80ae02a0ad1e7
nschutz <nschutz@student.42wolfsburg.de>
2024-02-06 13:25:21 +0100

    feat: CPP01 ex01 I am stupid but it is finished now

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -5,7 +5,7 @@ Zombie::Zombie(){
 }
 
 Zombie::~Zombie(){
-	std::cout << name << "Destroyed\n";
+	std::cout << name << " Destroyed\n";
 }
 
 void Zombie::announce(void){
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -3,7 +3,8 @@
 Zombie	*zombieHorde(int N, std::string name){
 	Zombie *Horde = new Zombie[N];
 
-	 while (N > 0){
+	N--;
+	 while (N >= 0){
 		 Horde[N].set_name(name);
 		 Horde[N].announce();
 		 N--;
