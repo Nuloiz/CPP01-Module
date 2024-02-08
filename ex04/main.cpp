8aa849eb2ea3fdaa41cf578b78ef4183bdfd0002
nschutz <nschutz@student.42wolfsburg.de>
2024-02-08 16:00:40 +0100

    feat CPP01: ex04 first version

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,5 +2,26 @@
 #include <iostream>
 
 int main(std::string filename, std::string s1, std::string s2){
-    ifstream in(filename, ios_base::in);
+    std::ifstream   inf(filename,);
+    std::ofstream   outf(filename + ".replace");
+    std::string     string;
+    int ls1;
+    int pos = -1;
+    int i = 0;
+    if (!inf){
+        std::cout << "Invalid Filename\n";
+        return (0);
+    }
+    std::getline(inf, string);
+    ls1 = s1.length();
+    while (string[++pos + ls1]){
+        if (&string[pos].compare(ls1, s2)){
+            outf << string.substr(&string[pos], i) << s2;
+            i = 0;
+            pos += ls1;
+        }
+        else
+            i++;
+    }
+    return (1);
 }
