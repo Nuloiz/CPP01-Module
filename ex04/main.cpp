b33899468bedc3d18abc66b433f436f2e5d6bd53
nschutz <nschutz@student.42wolfsburg.de>
2024-02-09 12:19:37 +0100

    feat CPP01: ex04 finished

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,6 +6,7 @@ void replace(std::string filename, std::string s1, std::string s2){
     std::ifstream   inf(filename.c_str());
     std::ofstream   outf(filename.append(".replace").c_str());
     std::string     string;
+    std::string     tmp;
     int ls1;
     int pos = -1;
     int i = 0;
@@ -13,20 +14,18 @@ void replace(std::string filename, std::string s1, std::string s2){
         std::cout << "Invalid Filename\n";
         return ;
     }
-    std::getline(inf, string);
+    while (std::getline(inf, tmp))
+        string.append(tmp + "\n");
     ls1 = s1.length();
     while (string[(++pos + ls1)]){
-        if (string.compare(pos, ls1, s2)){
-            outf << string.substr(pos, i);
-            std::cout << string.substr(pos, i);
-            std::cout << pos << i << ls1;
+        if (!string.compare(pos, ls1, s1)){
+            outf << string.substr(i, pos - i);
             outf << s2;
-            i = 0;
-            pos += ls1;
+            pos += ls1 - 1;
+            i = pos + 1;
         }
-        else
-            i++;
     }
+    outf << string.substr(i, pos - i);
     inf.close();
     outf.close();
 }
