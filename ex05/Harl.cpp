b611d3af65ba371b1c600fe30f7dddc884b3c42c
nschutz <nschutz@student.42wolfsburg.de>
2024-02-09 13:11:33 +0100

    feat CPP01: ex05 Harl

---
+++ ex05/Harl.cpp
@@ -0,0 +1,7 @@
+#include "Harl.hpp"
+
+Harl::Harl() {
+}
+
+Harl::~Harl() {
+}
---
+++ ex05/Harl.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Harl{
+public:
+
+    Harl();
+
+    void complain( std::string level );
+
+    ~Harl();
+
+private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+};
