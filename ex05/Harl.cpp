411df6a54939025f86fd98851adb0c726129a7b4
nschutz <nschutz@student.42wolfsburg.de>
2024-02-09 14:09:40 +0100

    feat CPP01: ex05 adding std::cout

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -5,3 +5,18 @@ Harl::Harl() {
 
 Harl::~Harl() {
 }
+
+void Harl::debug() {
+    std::cout << "[Debug] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void Harl::info() {
+    std::cout << "[Info] I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n";
+}
+
+void Harl::warning() {
+    std::cout << "[Warning] I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n";
+}
+void Harl::error() {
+    std::cout << "[Error] I’m sorry, I’m going to have to ask you to leave. We’ve had complaints about your behaviour.\n";
+}
