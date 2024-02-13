d71c0e60f07d880b9539386f7c6af067b6f71327
nschutz <nschutz@student.42wolfsburg.de>
2024-02-13 11:00:08 +0100

    feat CPP01: ex03 small quality of life change

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,12 +7,12 @@ int main(void){
 	std::string &stringREF = s;
 
 	std::cout << "Memory address:\n";
-	std::cout << &s << "\n";
-	std::cout << stringPTR << "\n";
-	std::cout << &stringREF << "\n";
+	std::cout << "s" << &s << "\n";
+	std::cout << "stringPTR" << stringPTR << "\n";
+	std::cout << "stringREF" << &stringREF << "\n";
 	std::cout << "Value:\n";
-	std::cout << s << "\n";
-	std::cout << *stringPTR << "\n";
-	std::cout << stringREF << "\n";
+	std::cout << "s" << s << "\n";
+	std::cout << "stringPTR" << *stringPTR << "\n";
+	std::cout << "stringREF" << stringREF << "\n";
 	return (1);
 }
